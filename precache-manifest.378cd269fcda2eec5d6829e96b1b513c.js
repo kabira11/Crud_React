8d0a000e2f291068b6d13d00b8569a6875e5abcb
kabir11 <pankajbhj@gmail.com>
2019-08-13 11:55:39 +0530

    Updates

---
+++ precache-manifest.378cd269fcda2eec5d6829e96b1b513c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b079aedbefac50864f0736c8835eaa2",
+    "url": "/Crud_React/index.html"
+  },
+  {
+    "revision": "7c63021e79691635c04b",
+    "url": "/Crud_React/static/css/2.4fa47c11.chunk.css"
+  },
+  {
+    "revision": "cf4c25a3f90f241be7f7",
+    "url": "/Crud_React/static/css/main.67c2e290.chunk.css"
+  },
+  {
+    "revision": "7c63021e79691635c04b",
+    "url": "/Crud_React/static/js/2.e0dab043.chunk.js"
+  },
+  {
+    "revision": "cf4c25a3f90f241be7f7",
+    "url": "/Crud_React/static/js/main.2db4c9cb.chunk.js"
+  },
+  {
+    "revision": "85ff65f70cbe103f5156",
+    "url": "/Crud_React/static/js/runtime~main.4fcb44db.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Crud_React/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Crud_React/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Crud_React/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Crud_React/static/media/notification.c0d3c94c.eot"
+  }
+]);
